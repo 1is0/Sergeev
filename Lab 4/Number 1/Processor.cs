d63b16ba68fecf687b5b8e2fb1f74a1cd3523342
evilgeniy <62379284+evilgeniy@users.noreply.github.com>
2020-04-05 10:12:47 +0300

    Create Processor.cs

---
+++ Lab 4/Number 1/Processor.cs
@@ -0,0 +1,19 @@
+  
+using System;
+using System.Runtime.InteropServices;
+namespace Unsafe
+{
+    class Processor
+    {
+        public short ProcessorArchitecture;
+        public uint PageSize;
+        public IntPtr MinimumApplicationAddress;
+        public IntPtr MaximumApplicationAddress;
+        public IntPtr ActiveProcessorMask;
+        public uint NumberOfProcessors;
+        public uint ProcessorType;
+        public uint AllocationGranularity;
+        public ushort ProcessorLevel;
+        public ushort ProcessorRevision;
+    }
+}
