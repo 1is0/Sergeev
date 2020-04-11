75317f5b470e8f55014eaecc09f0f0e1520ecd7a
evilgeniy <62379284+evilgeniy@users.noreply.github.com>
2020-04-11 11:41:35 +0300

    Delete NodeNumber.cpp

--- Lab Cplusplus/LR 4/NodeNumber.cpp
+++
@@ -1,9 +0,0 @@
-//---------------------------------------------------------------------------
-#pragma hdrstop
-#include "NodeNumber.h"
-NodeNumber :: NodeNumber(float n)
-{
-	num = n;
-}
-//---------------------------------------------------------------------------
-#pragma package(smart_init)
