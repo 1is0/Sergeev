2d26ef108b8ef6792225ae588101b75b4987e195
evilgeniy <62379284+evilgeniy@users.noreply.github.com>
2020-04-11 11:07:42 +0300

    Create NodeNumber.cpp

---
+++ Lab Cplusplus/LR 4/NodeNumber.cpp
@@ -0,0 +1,9 @@
+//---------------------------------------------------------------------------
+#pragma hdrstop
+#include "NodeNumber.h"
+NodeNumber :: NodeNumber(float n)
+{
+	num = n;
+}
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
