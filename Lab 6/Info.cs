3b3941e8f58053b31a4ef56d48f28533e0b691d9
evilgeniy <62379284+evilgeniy@users.noreply.github.com>
2020-05-02 13:21:30 +0300

    Create Info.cs

---
+++ Lab 6/Info.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Vihecle
+{
+    abstract class Information
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        protected string ID { get; set; }
+
+        public Information()
+        {
+            Name = "Porshe";
+            Country = "Italy";
+            ID = GenID();
+        }
+
+        public Information(string a, string b)
+        {
+            Name = a;
+            Country = b;
+            ID = GenID();
+        }
+
+        public static string GenID() => Guid.NewGuid().ToString();
+
+        public override string ToString() => $"Name: {Name}\nCountry: {Country}\nID: {ID}";
+    }
+}
